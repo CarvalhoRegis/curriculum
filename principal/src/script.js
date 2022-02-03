cdde3d822f90ea8f18171ac5306791204bc90816
Gabriel Carvalho Regis <gabriel-carvalho52@hotmail.com>
2022-02-02 23:43:03 -0300

    Div "sobre"

--- principal/src/script.js
+++ principal/src/script.js
@@ -59,3 +59,22 @@ function erase() {
         setTimeout(type, newTextDelay + 250);
     });
 }
+
+// sobre
+var i = 0;
+var tag = document.getElementById("text");
+var html = document.getElementById("text").innerHTML;
+
+var attr = tag.setAttribute("data", html);]
+var txt = tag.getAttribute("data");
+var speed = 170;
+
+function typeWrite() {
+
+    if(i <= txt.length) {
+        document.getElementById("text").innerHTML = txt.slice(0, i+1);
+        i++;
+        setTimeout(typewrite, speed);
+    }
+}
+typeWrite();
